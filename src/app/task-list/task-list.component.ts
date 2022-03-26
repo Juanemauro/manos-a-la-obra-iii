657e2e63ed17d88c5ae2e71b832210def70c2c1f
unknown <jmauro492@gmail.com>
2022-03-26 15:29:36 -0300

    Add-task for scrolled version. Task checked.

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -12,7 +12,24 @@ export class TaskListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  tasks: any[] = [{ id: 1, value: "Tarea 1" }, { id: 1, value: "Tarea 2" }, { id: 3, value: "Tarea 3" }]
+  tasks: any[] = [{ id: 1, value: "Tarea 1" },
+  { id: 1, value: "Tarea 2" },
+  { id: 3, value: "Tarea 3" },
+  { id: 4, value: "Tarea 4" },
+  { id: 5, value: "Tarea 5" },
+  { id: 6, value: "Tarea 6" },
+  { id: 7, value: "Tarea 7" },
+  { id: 8, value: "Tarea 8" },
+  { id: 9, value: "Tarea 9" },
+  { id: 10, value: "Tarea 10" },
+  { id: 11, value: "Tarea 11" },
+  { id: 12, value: "Tarea 12" },
+  { id: 13, value: "Tarea 13" },
+  { id: 14, value: "Tarea 14" },
+  { id: 15, value: "Tarea 15" },
+  { id: 16, value: "Tarea 16" },
+  { id: 17, value: "Tarea 17" },
+]
 
   // Add Task
   addTask(newTask: any) {
